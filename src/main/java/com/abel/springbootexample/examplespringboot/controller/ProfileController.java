bee9777cd214b4849ca5c35b988201df8036098a
Abel <abel@doku.com>
2018-07-16 10:06:33 +0700

    change the request mapping

--- src/main/java/com/abel/springbootexample/examplespringboot/controller/ProfileController.java
+++ src/main/java/com/abel/springbootexample/examplespringboot/controller/ProfileController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "doku/api/profile")
+@RequestMapping(value = "api/profile")
 public class ProfileController {
     @Autowired
     private ProfileService profileService;
